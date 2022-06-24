9585e914c4e04407c229915ba6dc702d3fe06c44
Chris Wallace <Chrisw1096@gmail.com>
2022-06-24 17:20:53 +1000

    added google api to JS, calling sucessfully, not appending yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 var tableBody = document.getElementById('searchResults');
 var fetchButton = document.getElementById('searchButton');
 
+// 
+// Example Fetch, feel free to delete of modify
+// 
+
 function getApi() {
   // fetch request gets a list of all the repos for the node.js organization
   var requestUrl = 'https://api.github.com/orgs/nodejs/repos';
@@ -32,3 +36,39 @@ function getApi() {
 }
 
 fetchButton.addEventListener('click', getApi);
+// 
+// 
+// 
+
+
+
+// Heres the Google API call. The data is logged to the console, however I'm still working on getting it to display. 
+// We may need to make a series of cards on the html and append the data to the cards
+// Either way, the search variable is "searchTerm", if the marvel api result that the user wants to see is set to "searchTerm", the two api's will talk. 
+
+function googleAPIfunction(){
+
+var googleAPIKey = "AIzaSyD7sP34KCHB1bSqJZEouHRFLhFVPC9pu7w";
+var searchTerm = "apple";
+var queryURL = "https://www.googleapis.com/customsearch/v1?key="+googleAPIKey+"&cx=716b6da6cc16aa14e&q="+searchTerm;
+// &callback=hndlr"
+
+
+    fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)})
+
+function hndlr(response) {
+  for (var i = 0; i < response.items.length; i++) {
+    var item = response.items[i];
+    // Make sure HTML in item.htmlTitle is escaped.
+    document.getElementById("content").append(
+      document.createElement("br"),
+      document.createTextNode(item.htmlTitle)
+  );
+}}
+ };
+googleAPIfunction();
